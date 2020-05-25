21b8d18bfaee396c8c40eaabc90fc5d9f8d6b226
Yudhistira Arya <746670+yudhistira-arya@users.noreply.github.com>
2020-05-25 23:53:00 +0800

    minor refactoring

--- restful/server/main.go
+++ restful/server/main.go
@@ -11,6 +11,24 @@ import (
 
 const meteoriteJsonFile = "y77d-th95.json"
 
+type Meteorite struct {
+	Name        string      `json:"name"`
+	Id          string      `json:"id"`
+	NameType    string      `json:"nametype"`
+	RecClass    string      `json:"recclass"`
+	Mass        string      `json:"mass"`
+	Fall        string      `json:"fall"`
+	Year        string      `json:"year"`
+	Reclat      string      `json:"reclat"`
+	Reclong     string      `json:"reclong"`
+	Geolocation Geolocation `json:"geolocation"`
+}
+
+type Geolocation struct {
+	Type        string    `json:"type"`
+	Coordinates []float64 `json:"coordinates"`
+}
+
 var meteorites []Meteorite
 
 func init() {
@@ -32,34 +50,16 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8090", nil))
 }
 
-func handleError(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-type Meteorite struct {
-	Name        string      `json:"name"`
-	Id          string      `json:"id"`
-	NameType    string      `json:"nametype"`
-	RecClass    string      `json:"recclass"`
-	Mass        string      `json:"mass"`
-	Fall        string      `json:"fall"`
-	Year        string      `json:"year"`
-	Reclat      string      `json:"reclat"`
-	Reclong     string      `json:"reclong"`
-	Geolocation Geolocation `json:"geolocation"`
-}
-
-type Geolocation struct {
-	Type        string    `json:"type"`
-	Coordinates []float64 `json:"coordinates"`
-}
-
 func getMeteors(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Content-Type", "application/json; charset=utf-8")
 	writer.WriteHeader(http.StatusOK)
 	responseByte, err := json.Marshal(&meteorites)
 	handleError(err)
 	_, _ = writer.Write(responseByte)
+}
+
+func handleError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
